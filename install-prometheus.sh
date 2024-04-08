7f53f279d286bdbfa631d4de70202c1e826fe1b6
kiran45git <163430744+kiran45git@users.noreply.github.com>
2024-04-08 18:07:35 +0530

    Update install-prometheus.sh

--- install-prometheus.sh
+++ install-prometheus.sh
@@ -3,14 +3,14 @@ sudo mkdir /etc/prometheus
 sudo mkdir /var/lib/prometheus
  
 wget  https://github.com/prometheus/prometheus/releases/download/v2.51.1/prometheus-2.51.1.linux-amd64.tar.gz
-tar -xvf prometheus-2.23.0.linux-amd64.tar.gz
-sudo cp prometheus-2.23.0.linux-amd64/prometheus /usr/local/bin
-sudo cp prometheus-2.23.0.linux-amd64/promtool /usr/local/bin
-sudo cp -r prometheus-2.23.0.linux-amd64/consoles /etc/prometheus/
-sudo cp -r prometheus-2.23.0.linux-amd64/console_libraries /etc/prometheus
-sudo cp prometheus-2.23.0.linux-amd64/promtool /usr/local/bin/
+tar -xvf prometheus-2.51.1.linux-amd64.tar.gz
+sudo cp prometheus-2.51.1.linux-amd64/prometheus /usr/local/bin
+sudo cp prometheus-2.51.1.linux-amd64/promtool /usr/local/bin
+sudo cp -r prometheus-2.51.1.linux-amd64/consoles /etc/prometheus/
+sudo cp -r prometheus-2.51.1.linux-amd64/console_libraries /etc/prometheus
+sudo cp prometheus-2.51.1.linux-amd64/promtool /usr/local/bin/
 
-rm -rf prometheus-2.23.0.linux-amd64.tar.gz prometheus-2.19.0.linux-amd64
+rm -rf prometheus-2.51.1.linux-amd64.tar.gz prometheus-2.19.0.linux-amd64
 sudo cp prometheus.yml /etc/prometheus/
 sudo cp prometheus.service /etc/systemd/system/prometheus.service
 
